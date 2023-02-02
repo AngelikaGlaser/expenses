e1eb23b7440801cef600e7faac734850d7d725d2
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-02-02 14:31:01 +0200

    Added clickHandler that set title value to 'Updated'.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,21 @@
-import './ExpenseItem.css'
-import './ExpenseDate';
+import React, {useState} from "react";
+import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title)
+
     const clickHandler = () => {
-        console.log('clicked')
+        setTitle('Updated')
+        console.log(title)
     }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.expenseData.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
